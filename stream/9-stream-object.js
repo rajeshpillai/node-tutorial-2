a9789e24801acd66605cd254d7385a1cb70eb1fc
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-02-24 11:30:35 +0530

    Stream Object

--- stream/9-stream-object.js
+++ stream/9-stream-object.js
@@ -8,6 +8,17 @@ Here’s a simple example to demonstrate that. The following combination of tran
 streams makes for a feature to map a string of comma-separated values into a 
 JavaScript object. So “a,b,c,d” becomes {a: b, c: d}.
 
+We pass the input string (for example, “a,b,c,d”) through commaSplitter which pushes an 
+array as its readable data ([“a”, “b”, “c”, “d”]). Adding the readableObjectMode 
+flag on that stream is necessary because we’re pushing an object there, not a string.
+
+We then take the array and pipe it into the arrayToObject stream. We need a 
+writableObjectMode flag to make that stream accept an object. It’ll also push 
+an object (the input array mapped into an object) and that’s why we also needed 
+the readableObjectMode flag there as well. The last objectToString stream accepts 
+an object but pushes out a string, and that’s why we only needed a 
+writableObjectMode flag there. The readable part is a normal string (the stringified object).
+
 */
 
 const { Transform } = require('stream');
