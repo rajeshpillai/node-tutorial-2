bf3150383a418244d5f21cb1c0681b066a029351
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-02-24 12:54:36 +0530

    Path -> More examples

--- 2-path.js
+++ 2-path.js
@@ -4,3 +4,11 @@ let directories = ["bin","output", "public"];
 let directory = directories.join(path.sep);
 
 console.log("directory path: ", directory);
+
+// More examples
+var fileName = "/app/controller/home.js";
+var file = path.basename(fileName);
+var extension = path.extname(fileName);
+
+console.log("Filename: ", file);
+console.log("Ext: ", extension);
